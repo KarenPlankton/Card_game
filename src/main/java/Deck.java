9607bee5d1d88277eccaa59047789e2ccf4cf45b
Anna <elbereth_spencer@hotmail.com>
2020-09-24 12:11:59 +0100

    first commit

---
+++ src/main/java/Deck.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck
+  {
+      private ArrayList<Card> cards;
+
+
+      public Deck(){
+          this.cards=new ArrayList<Card>();
+
+      }
+
+      public void createDeck(){
+
+          for (Suit suit : Suit.values()){
+              for(Rank rank :Rank.values()){
+                  Card card = new Card(suit,rank);
+                  this.cards.add(card);
+              }
+          }
+      }
+
+      public void shuffleDeck(){
+          Collections.shuffle(this.cards);
+  }
+}
